0cf7aae7399ab84a77e271a57bc8706b1d607f96
Joe M <joe.mireles@burnsoft.net>
2020-07-05 16:45:33 -0400

    Updated properties

--- BurnSoft.Security.RegularEncryption/Properties/AssemblyInfo.cs
+++ BurnSoft.Security.RegularEncryption/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BurnSoft.Security.RegularEncryption")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BurnSoft.Security.RegularEncryption simplifies some of the other encryption options from System.Security.Cryptography To make quick and easy calls to encrypt and decrypt information")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("www.burnsoft.net")]
 [assembly: AssemblyProduct("BurnSoft.Security.RegularEncryption")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © www.burnsoft.net 2020")]
+[assembly: AssemblyTrademark("www.burnsoft.net")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
