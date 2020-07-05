721d4c2037b0c48896ff45db5f68ec26653bb546
Joe M <joe.mireles@burnsoft.net>
2020-07-05 16:36:07 -0400

    Added sha1 encrypt

---
+++ BurnSoft.Security.RegularEncryption/SHA/One.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace BurnSoft.Security.RegularEncryption.SHA
+{
+     public class One
+    {
+        /// <summary>
+        /// Pass string to Encrypt using SHA
+        /// </summary>
+        /// <param name="Data"></param>
+        /// <returns></returns>
+        public static string Encrypt(string Data)
+        {
+            SHA1Managed shaM = new SHA1Managed();
+            Convert.ToBase64String(shaM.ComputeHash(Encoding.ASCII.GetBytes(Data)));
+            byte[] eNC_data = ASCIIEncoding.ASCII.GetBytes(Data);
+            string eNC_str = Convert.ToBase64String(eNC_data);
+            return eNC_str;
+        }
+
+    }
+}
