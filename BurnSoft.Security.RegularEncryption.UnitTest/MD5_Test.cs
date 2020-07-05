ce318f025e8a88b9eb1b4406d9cf859c6f8246d1
Joe M <joe.mireles@burnsoft.net>
2020-07-05 16:33:47 -0400

    Added to md5 and created unit test

---
+++ BurnSoft.Security.RegularEncryption.UnitTest/MD5_Test.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BurnSoft.Security.RegularEncryption.UnitTest
+{
+    [TestClass]
+    public class MD5_Test
+    {
+        /// <summary>
+        /// Gets or sets the test context.
+        /// </summary>
+        /// <value>The test context.</value>
+        public TestContext TestContext { get; set; }
+        /// <summary>
+        /// The test text
+        /// </summary>
+        private string _testText;
+        /// <summary>
+        /// The test text URL
+        /// </summary>
+        private string _testTextUrl;
+        /// <summary>
+        /// The test text session identifier
+        /// </summary>
+        private string _testTextSessionId;
+        /// <summary>
+        /// The test text verify m d5
+        /// </summary>
+        private string _testTextVerifyMd5;
+        /// <summary>
+        /// The test text verifymd52
+        /// </summary>
+        private string _testTextVerifymd52;
+        /// <summary>
+        /// The test text verify sha
+        /// </summary>
+        private string _testTextVerifySha;
+        /// <summary>
+        /// Initializes this instance.
+        /// </summary>
+        [TestInitialize]
+        public void Init()
+        {
+            _testText = "hashThis";
+            _testTextUrl = "http://www.burnsoft.net/test.aspx";
+            _testTextSessionId = "428095ur3948htirhgkerhg04t34t";
+            _testTextVerifyMd5 = "51-CA-F8-84-41-C7-7A-C1-49-11-A3-D1-03-3A-C1-FE";
+            _testTextVerifymd52 = "826a37cd6191450b00488f8d1ee34127";
+            _testTextVerifySha = "aGFzaFRoaXM=";
+
+        }
+        /// <summary>
+        /// Defines the test method EncryptMd5Test.
+        /// </summary>
+        [TestMethod, TestCategory("Encrypt - MD5")]
+        public void EncryptMd5Test2()
+        {
+
+            string value = BurnSoft.Security.RegularEncryption.MD5.Do.Encrypt(_testText);
+            TestContext.WriteLine(value);
+            Assert.IsTrue(value.Equals(_testTextVerifymd52));
+        }
+    }
+}
---
+++ BurnSoft.Security.RegularEncryption/MD5/Do.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace BurnSoft.Security.RegularEncryption.MD5
+{
+    public class Do
+    {
+        /// <summary>
+        /// Pass a string to Encrypt using MD5
+        /// </summary>
+        /// <param name="cleanString"></param>
+        /// <returns></returns>
+        public static string Encrypt(string cleanString)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            System.Security.Cryptography.MD5 md5Hash = System.Security.Cryptography.MD5.Create();
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(cleanString));
+            // Create a new Stringbuilder to collect the bytes
+            // // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+            // Loop through each byte of the hashed data
+            // // and format each one as a hexadecimal string
+            // .
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+    }
+}
